800c195c0a074398bdaf1b74d20d0a2fcce03d3e
Patrick N A <patrickdevtech@gmail.com>
2026-09-12 12:35:36 -0300

    feat: estrutura Maven multi-modulo com arquitetura hexagonal

---
+++ notificacao-service/src/main/java/br/com/fiap/vagazero/notificacao/NotificacaoServiceApplication.java
@@ -0,0 +1,12 @@
+package br.com.fiap.vagazero.notificacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotificacaoServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NotificacaoServiceApplication.class, args);
+    }
+}
